2a92ebc79ae78483f7102221f14af536f76be3c8
Scratchers7 <journeynikola@gmail.com>
2025-03-19 19:20:46 -0600

    added_quadrant_variable_to_ThreadOperation

--- matrixAddition/src/matrixAddition/ThreadOperation.java
+++ matrixAddition/src/matrixAddition/ThreadOperation.java
@@ -3,11 +3,13 @@ package matrixAddition;
 public class ThreadOperation extends Thread{
 	private int[][] matrix1;
 	private int[][] matrix2;
+	private int quadrant;
 	
-	public ThreadOperation(int[][] matrix1, int[][] matrix2)
+	public ThreadOperation(int[][] matrix1, int[][] matrix2, int quadrant)
 	{
 		this.matrix1 = matrix1;
 		this.matrix2 = matrix2;
+		this.quadrant = quadrant;
 	}
 	
 	public void run()
