ee2b7830340a43d6406621bbfdc60467baa1520b
Scratchers7 <journeynikola@gmail.com>
2025-03-19 19:09:55 -0600

    created_ThreadOperation_class_for_adding_matrixes_and_Main_to_get_and_divide_the_matrixes

---
+++ matrixAddition/src/matrixAddition/ThreadOperation.java
@@ -0,0 +1,17 @@
+package matrixAddition;
+
+public class ThreadOperation extends Thread{
+	private int[][] matrix1;
+	private int[][] matrix2;
+	
+	public ThreadOperation(int[][] matrix1, int[][] matrix2)
+	{
+		this.matrix1 = matrix1;
+		this.matrix2 = matrix2;
+	}
+	
+	public void run()
+	{
+		
+	}
+}
