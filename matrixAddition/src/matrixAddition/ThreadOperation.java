745ea43ef6a8c50d1a669ab4c8c1457f6d3673e5
Scratchers7 <journeynikola@gmail.com>
2025-03-23 15:36:44 -0600

    Added comments to ThreadOperation

--- matrixAddition/src/matrixAddition/ThreadOperation.java
+++ matrixAddition/src/matrixAddition/ThreadOperation.java
@@ -1,12 +1,13 @@
 package matrixAddition;
 
 public class ThreadOperation extends Thread{
-	private int[][] matrix1;
-	private int[][] matrix2;
-	private int quadrant;
+	private int[][] matrix1;//holds first matrix to add
+	private int[][] matrix2;//holds second matrix to add
+	private int quadrant;//holds which quadrant of the matrix this thread is adding up.
 	
 	public ThreadOperation(int[][] matrix1, int[][] matrix2, int quadrant)
 	{
+		//instantiates a new thread to add the matrixes
 		this.matrix1 = matrix1;
 		this.matrix2 = matrix2;
 		this.quadrant = quadrant;
@@ -14,6 +15,6 @@ public class ThreadOperation extends Thread{
 	
 	public void run()
 	{
-		
+		//TODO: make it add the quadrant of matrix assigned.
 	}
 }
